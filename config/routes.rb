5f58d4670719e062d3145f688bb1f1e256bab0aa
Lisa Lu <lisalu092@gmail.com>
2014-11-10 00:06:53 +0100

    Cours3 Partie 1 avec erreur

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+  post 'shows/:id/book' => 'shows#book'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
